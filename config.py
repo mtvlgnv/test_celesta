539bc2b564022062d7467eb26891e5e0bb7c8d9e
mtvlgnv <Matveylgnv2021@gmail.com>
2025-11-04 15:51:10 +0100

    Add data processing feature with code quality issues for Celesta review
    This PR intentionally contains:
    - Deep nesting
    - TODO/FIXME comments
    - Debug code
    - Long functions
    - Commented code
    - For testing Celesta's code review capabilities

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuration file with commented code
+def get_config():
+    # Commented out code (Celesta will flag this!)
+    # if production:
+    #     return "prod"
+    # else:
+    #     return "dev"
+    return "dev"
